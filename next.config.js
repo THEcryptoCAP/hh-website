050f1a409fb329b167bb0c633f47d3c6ebe67c68
Avinash <cryptocapinoid@gmail.com>
2025-09-16 19:35:40 +0530

    https redirect

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  turbopack: { root: __dirname }
+  turbopack: { root: __dirname },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
+        destination: 'https://hanshillsco.com/:path*',
+        permanent: true
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
